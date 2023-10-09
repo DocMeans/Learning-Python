e154c4cc688e18c20d74a893c666d9ce326cee49
DocMeans <75758516+DocMeans@users.noreply.github.com>
2023-10-08 23:53:42 -0400

    listing data tables now
    working with the combo boxes. next up adding tables to the database from the app

--- KEIWIP/trucks.py
+++ KEIWIP/trucks.py
@@ -13,57 +13,78 @@ class MyDialog(wx.Dialog):
 
         wx.Dialog.__init__(self, None, title="Model and Year")
 
-        lbl = wx.StaticText(self, label='Enter Forklift Info', pos=(120, 10))
+        lbl = wx.StaticText(self, label='Enter Forklift Info', pos=(50, 10))
 
-        self.serial_number = wx.TextCtrl(self, -1, '', pos=(115, 40))
-        wx.StaticText(self, -1, 'Serial Number', pos=(230, 40))
+        self.serial_number = wx.TextCtrl(self, -1, '', pos=(50, 40))
+        wx.StaticText(self, -1, 'Serial Number', pos=(175, 40))
 
-        self.year = wx.TextCtrl(self, -1, '', (115, 80))
-        wx.StaticText(self, -1, 'Year', (230, 80))
+        self.year = wx.TextCtrl(self, -1, '', (50, 80))
+        wx.StaticText(self, -1, 'Year', (175, 80))
 
-        self.model = wx.TextCtrl(self, -1, '', (115, 120))
-        wx.StaticText(self, -1, 'Model', (230, 120))
+        self.model = wx.TextCtrl(self, -1, '', (50, 120))
+        wx.StaticText(self, -1, 'Model', (175, 120))
+
+        self.label = wx.StaticText(self, label="Choose Make", pos=(200, 10))
+        make = ["toyota", "kamatsu", "hyster", "mitsubishi", "catepillar"]
+        self.comboBox = wx.ComboBox(self, choices=make, pos=(275, 10) )
 
         okBtn = wx.Button(self, id=wx.ID_OK, pos=(135, 170))  # add wx.OK button
 
 
 class DataList(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(480, 270))
+        wx.Frame.__init__(self, parent, id, title, size=(480, 300))
         panel = wx.Panel(self, -1)
-
-        self.table_name = wx.StaticText(panel, -1, 'Forklift', pos=(50, 5))
-        self.list = wx.ListCtrl(panel, -1, style=wx.LC_REPORT, pos=(20, 30), size=(370, -1))
+        
+        self.list = wx.ListCtrl(panel, -1, style=wx.LC_REPORT, pos=(20, 50), size=(370, -1))
         # set up columns
         self.list.InsertColumn(0, 'Serial number', width=90)
         self.list.InsertColumn(1, 'Year', width=90)
         self.list.InsertColumn(2, 'Model', width=90)
-        #self.list.InsertColumn(3, 'email', wx.LIST_FORMAT_RIGHT, 140)
+
+        # setup combo box for the main screen
+        self.label = wx.StaticText(panel, -1, label="Choose Make", pos=(150, 5))
+        make = ["toyota", "kamatsu", "hyster", "mitsubishi", "catepillar"]
+        self.comboBox = wx.ComboBox(panel, -1, choices=make, pos=(250, 5) )
+        self.table_name = wx.StaticText(panel, -1, 'Forklift', pos=(50, 5))
 
         # set up buttons
-        display = wx.Button(panel, -1, 'Display', size=(-1, 30), pos=(20, 190))
-        insert = wx.Button(panel, -1, 'Insert Truck', size=(-1, 30), pos=(100, 190))
-        cancel = wx.Button(panel, -1, 'Cancel', size=(-1, 30), pos=(200, 190))
-        search = wx.Button(panel, -1, 'Search', size=(-1, 30), pos=(280, 190))
+        display = wx.Button(panel, -1, 'Display', size=(-1, 30), pos=(20, 215))
+        insert = wx.Button(panel, -1, 'Insert Truck', size=(-1, 30), pos=(100, 215))
+        cancel = wx.Button(panel, -1, 'Cancel', size=(-1, 30), pos=(200, 215))
+        search = wx.Button(panel, -1, 'Search', size=(-1, 30), pos=(280, 215))
 
         display.Bind(wx.EVT_BUTTON, self.OnDisplay )  # bind buttons to event handlers
         insert.Bind(wx.EVT_BUTTON, self.OnAdd )
         cancel.Bind(wx.EVT_BUTTON, self.OnCancel)
         search.Bind(wx.EVT_BUTTON, self.search)
         self.Centre()
-    
-    def getAllData(self):   # helper function, displays whole table
 
-        self.list.DeleteAllItems()    # empty the list control
-        con = db.connect('trucks.sqlite')  # connect to db
+    def getAllData(self):
+        # Get the selected make from the combo box
+        selected_make = self.comboBox.GetValue()
+
+        # Construct the SQL query based on the selected make
+        query = f"SELECT * FROM {selected_make}"
+
+        self.list.DeleteAllItems()  # Empty the list control
+        con = db.connect('trucks.sqlite')  # Connect to the database
         cur = con.cursor()
-        cur.execute('SELECT * FROM forklift')
-        results = cur.fetchall()
-        
-        for row in results:
-            self.list.Append(row)  # add record to list control
-        cur.close()
-        con.close()
+
+        try:
+            cur.execute(query)
+            results = cur.fetchall()
+
+            for row in results:
+                self.list.Append(row)  # Add records to the list control
+
+        except db.Error as error:
+            dlg = wx.MessageDialog(self, str(error), 'Error occurred')
+            dlg.ShowModal()  # Display error message
+
+        finally:
+            cur.close()
+            con.close()
 
     def OnDisplay(self, event):
 
@@ -82,13 +103,23 @@ class DataList(wx.Frame):
             yr = dlg.year.GetValue()
             mod = dlg.model.GetValue()
 
+
         if sNum != "" and yr != "" and mod != "":   # only if no blank values
 
             try:
                 con = db.connect('trucks.sqlite')  # connect to db
                 cur = con.cursor()
-
-                sql = "INSERT INTO forklift VALUES (?, ?, ?)"
+                #["toyota", "kamatsu", "hyster", "mitsubishi", "catepillar"]
+                if dlg.comboBox.GetValue() == "toyota":
+                    sql = "INSERT INTO toyota VALUES (?, ?, ?)"
+                if dlg.comboBox.GetValue() == "kamatsu":
+                    sql = "INSERT INTO kamatsu VALUES (?, ?, ?)"
+                # if dlg.comboBox.GetValue() == "hyster":
+                #     sql = "INSERT INTO hyster VALUES (?, ?, ?)"
+                # if dlg.comboBox.GetValue() == "mitsubishi":
+                #     sql = "INSERT INTO mitsubishi VALUES (?, ?, ?)"
+                # if dlg.comboBox.GetValue() == "catepillar":
+                #     sql = "INSERT INTO catepillar VALUES (?, ?, ?)"         
 
                 cur.execute(sql, (sNum, yr, mod))
                 con.commit()          # complete the transaction
@@ -106,7 +137,6 @@ class DataList(wx.Frame):
 
     def OnCancel(self, event):
         self.Close()  # exit program
-
 
 app = wx.App()
 dl = DataList(None, -1, 'Insert Into Forklift')
